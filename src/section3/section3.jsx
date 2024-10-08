66b9d5cfd3550729dae61376e55e7e5da030f3a5
Taha Mhb <tahamhb12@gmail.com>
2024-10-08 13:32:59 +0100

    first commit

--- src/section3/section3.jsx
+++ src/section3/section3.jsx
@@ -17,15 +17,15 @@ const Section3 = () => {
                 <div className="first">
                     <div className="img"></div>
                     <div className="infos">
-                        <h2>Standards of Excellence</h2>
-                        <p>our members adhere to the highest standards of market research. Our code of conduct ensures your work is ethical, transparent, and reliable.</p>
+                        <h2>Professional Development</h2>
+                        <p>We provide continuous learning opportunities and keep you up to date with industry advancements.</p>
                     </div>
                 </div>
                 <div className="first">
                     <div className="img"></div>
                     <div className="infos">
-                        <h2>Standards of Excellence</h2>
-                        <p>our members adhere to the highest standards of market research. Our code of conduct ensures your work is ethical, transparent, and reliable.</p>
+                        <h2>Networking and Collaboration</h2>
+                        <p>Access a network of top researchers, data analysts and decision-makers. Collaborate,  share knowledge and grow alongside peers.</p>
                     </div>
                 </div>
             </div>
